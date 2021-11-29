2a1ed8db1c5b0f7126b6518e7f9e416644b7392e
JCGG-99977 <1848514604@qq.com>
2021-11-29 10:57:17 +0800

    初始化

---
+++ src/components/Homepage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="homepage">
+
+  </div>
+</template>
+
+<script>
+export default {
+name:'homepage',
+data(){
+    return{
+
+    }
+},
+created(){},
+methods:{}
+}
+</script>
+
+<style>
+.homepage{
+    width: 100%;
+    height: 100%;
+    background-color: aqua;
+}
+</style>
